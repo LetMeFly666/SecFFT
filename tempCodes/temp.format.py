db4ea4a524a7a99fd5c53ab52382b78c23836245
LetMeFly666 <814114971@qq.com>
2024-09-19 10:21:00 +0800

    tmp: before merge(pr of) branch z.001.tip-adapter
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- tempCodes/temp.format.py
+++
@@ -1,9 +0,0 @@
-'''
-Author: LetMeFly
-Date: 2024-09-09 19:33:37
-LastEditors: LetMeFly
-LastEditTime: 2024-09-09 19:34:34
-'''
-a = 1
-f = 3.2
-print(f'{a:7}\t{f:10f}')
--- tempCodes/temp.print.py
+++
@@ -1,8 +0,0 @@
-'''
-Author: LetMeFly
-Date: 2024-09-12 23:19:41
-LastEditors: LetMeFly
-LastEditTime: 2024-09-12 23:20:03
-'''
-for i in range(50):
-    print(i, end=',')
